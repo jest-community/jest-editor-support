e29a1f49a2f3c379c70cdae239c9545620b5dde7
Ross Knudsen <rossknudsen@gmail.com>
2020-01-27 17:28:31 +1300

    Update .eslintrc.js
    Co-Authored-By: Sean Poulter <sean.poulter+gh@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   },
   extends: [
     'airbnb-typescript/base',
-    'airbnb/hooks',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
