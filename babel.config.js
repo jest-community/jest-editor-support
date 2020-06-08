7b3d8df6fd605e2240be31e51b3c173a4846bf69
Ross Knudsen <rossknudsen@gmail.com>
2020-06-08 21:16:29 +1200

    fix: babel config to accomodate new language feature

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      { useBuiltIns: 'usage', corejs: '3' }
-    ],
-    '@babel/flow',
-    '@babel/typescript'
-  ],
+  presets: [['@babel/preset-env', {useBuiltIns: 'usage', corejs: '3'}], '@babel/flow', '@babel/typescript'],
+  plugins: ['@babel/proposal-class-properties'],
 };
